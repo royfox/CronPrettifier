d02170c888a2edcdac8e5919ef0088b4665d4611
Roy Fox <roysfox@gmail.com>
2013-04-15 23:02:16 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+A node script that takes crontab data and turns it into a more readable format, with upcoming tasks sorted by time.
+By default it will display all tasks in the next 24 hours, so long as they appear less than 10 times. 
+
+Dependencies:
+
+- [https://github.com/harrisiirak/node-cron-parser](https://github.com/harrisiirak/node-cron-parser)
+- [https://github.com/substack/node-optimist](https://github.com/substack/node-optimist)
+
+Setup:
+
+	npm install optimist
+	npm install cron-parser
+
+Example usage:
+
+	node app.js -f cronfile 
---
+++ app.js
@@ -0,0 +1,102 @@
+var 
+	parser = require('cron-parser'),
+	fs  = require("fs"),    
+	argv = require('optimist')
+        .demand('f')
+        .alias('f', 'file')
+		.describe('f', 'Load a file')
+        .default('threshold', 10)
+        .alias('t', 'threshold')
+		.describe('threshold', 'Tasks that appear more often than the threshold will be excluded')
+		.default('days', 1)
+		.describe('days','Number of days to display')
+        .argv;
+
+var 
+	keys = {},
+	lastComment = null;
+	endDate = new Date();
+
+endDate.setDate(endDate.getDate() + argv.days) //endDate should now be argv.days ahead of the current date
+
+var options = {
+  currentDate: new Date(),
+  endDate: endDate
+};
+
+/**
+  * Run through each of the file, and if it looks like a CRON task, try and parse it
+  * If it looks like a comment, save it and attach it to the next cron task
+  * Ignore lines that are just whitespace
+  */
+fs.readFileSync(argv.file).toString().split('\n').forEach(function (line) { 
+    if(line.indexOf("#") !== -1){
+		//comment
+    	lastComment = line.replace("#", "");
+    } else if(line.replace(/^\s\s*/, '').replace(/\s\s*$/, '').length == 0){
+    	//ignore whitespace lines
+    } else {
+    	//cron task
+		var pieces = line.split(/\s+/g);
+		var cronPieces = [];
+		var expressionPieces = [];
+
+		for(var i in pieces){
+			if(i < 5){
+				cronPieces.push(pieces[i]);
+			} else {
+				expressionPieces.push(pieces[i]);
+			}
+		}
+		parse(cronPieces.join(" "), expressionPieces.join(" "), lastComment);
+		lastComment = null;
+    }
+});
+
+
+/**
+ * @param string cronTime A cron string, of the form '* * * * *'
+ * @param string cronTask The expression to be executed
+ * @param string comment A comment about the task
+ */
+function parse(cronTime, cronTask, comment){
+	parser.parseExpression(cronTime, options, function(err, interval) {
+	  if (err) {
+	    console.log('Error: ' + err.message);
+	    return;
+	  }
+	  while (true) {
+	    try {
+			if(!keys[cronTask]){
+				keys[cronTask] = [];
+			}
+			keys[cronTask].push({
+				'time': interval.next(),
+				'task': cronTask,
+				'comment': comment
+			});
+	    } catch (e) {
+	        break;
+	    }
+	  }
+	});
+}
+
+
+//ignore events that occur more often than the threshold
+//stick the rest in a single array
+var events = [];
+for(var index in keys){
+	if(keys[index].length < argv.threshold){
+		events = events.concat(keys[index]);
+	}
+}
+
+//sort by time
+events.sort(function(a, b){
+	return a.time.getTime() - b.time.getTime();
+});
+
+for(var index in events){
+	console.log(events[index].time.toLocaleString() + ': ' + events[index].comment + ' | (' + events[index].task + ')');
+}
